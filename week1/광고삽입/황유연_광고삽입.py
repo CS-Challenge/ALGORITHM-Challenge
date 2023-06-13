aef0866cd0852f4f50e2c35e4f062bdb1e6e6255
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-06-13 18:01:35 +0900

    황유연: 광고삽입(23.06.13)

--- week1/광고삽입/황유연_광고삽입.py
+++ week1/광고삽입/황유연_광고삽입.py
@@ -1,4 +1,3 @@
-
 def time_to_sec(time):
     h, m, s = map(int, time.split(':'))
     return h * 3600 + m * 60 + s
@@ -13,50 +12,33 @@ def sec_to_time(start_time_to_sec):
 
 
 def solution(play_time, adv_time, logs):
-    answer = ''
-    if play_time == adv_time:
-        return "00:00:00"
-    play_time_to_sec = time_to_sec(play_time)  # 상영 시간
-    adv_time_to_sec = time_to_sec(adv_time)  # 광고 시간
-    # print(play_time_to_sec)
-    logs_to_sec = []
+    play_time = time_to_sec(play_time)  # 상영 시간
+    adv_time = time_to_sec(adv_time)  # 광고 시간
+    dp = [0 for _ in range(play_time + 1)]
+
     for log in logs:
         start_time, end_time = log.split('-')
-        start_time_to_sec = time_to_sec(start_time)
-        end_time_to_sec = time_to_sec(end_time)
-        logs_to_sec.append([start_time_to_sec, end_time_to_sec])
-    logs_to_sec.sort(key=lambda x: (x[1], x[0]))
-    # print(logs_to_sec)
-    ori_s = 0
-    ori_e = 0
-    terms = []
-    tmp = 0
-    acc_time = 0
-    # 겹치는 구간 구하기
-    for i in range(len(logs_to_sec)):
-        s, e = logs_to_sec[i]
-        if s <= ori_e:
-            tmp += 1
-            if ori_e - s > adv_time_to_sec:
-                acc_time += adv_time
-            else:
-                acc_time += ori_e - s
-            if i == len(logs_to_sec) - 1:
-                terms.append([acc_time, ori_s, ori_e, tmp])
-        else:
-            if tmp != 0:
-                terms.append([acc_time, ori_s, ori_e, tmp])
-            tmp = 1
-            acc_time = 0
-            ori_e = e
-            if e - adv_time_to_sec > 0:  # 끝점 기준 광고 시간만큼 구간을 정해둔다.
-                ori_s = max(e - adv_time_to_sec, s)
-            else:
-                ori_s = s
-    terms.sort(key=lambda x: (-x[0], x[1]))
-    if terms:
-        answer = sec_to_time(terms[0][1])
-    # print(terms)
+        start_time = time_to_sec(start_time)
+        end_time = time_to_sec(end_time)
+        dp[start_time] += 1
+        dp[end_time] -= 1
+    for i in range(1, play_time):
+        dp[i] += dp[i - 1]
+    for i in range(1, play_time):
+        dp[i] += dp[i - 1]
+    max_view = 0
+    max_start_time = 0
 
-    return answer
+    for pos in range(adv_time - 1, play_time):
+        if pos >= adv_time:
+            if max_view < dp[pos] - dp[pos - adv_time]:
+                max_view = dp[pos] - dp[pos - adv_time]
+                max_start_time = pos - adv_time + 1
+        else:
+            if max_start_time < dp[pos]:
+                max_view = dp[pos]
+                max_start_time = pos - adv_time + 1
+
+    return sec_to_time(max_start_time)
+
 
