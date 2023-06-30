ea4224d093b1b3b5d705d4e5afcd1b0f06453a8b
YEONI <89332391+yoooyeon@users.noreply.github.com>
2023-06-30 11:52:38 +0900

    황유연: 미로 탈출 명령어 문제풀이 수정(23.06.30)

--- week3/미로_탈출_명령어/황유연_미로_탈출_명령어.py
+++ week3/미로_탈출_명령어/황유연_미로_탈출_명령어.py
@@ -1,26 +1,28 @@
 from collections import deque
 
-
+# bfs 풀이
+# 방문 처리가 필요하지 않아 따로 맵을 만들 필요가 없다.
 def solution(n, m, x, y, r, c, k):
-    q = deque([(x - 1, y - 1, '', 0)])
-    dirs = ['d', 'l', 'r', 'u'] # 알파벳 순서대로
+    q = deque([(x, y, '', 0)])
+    dirs = ['d', 'l', 'r', 'u']  # 알파벳 순서대로
     dx, dy = [1, 0, 0, -1], [0, -1, 1, 0]
     while q:
-        x, y, tmp, cnt = q.popleft()
-        if cnt > k:
-            continue
-        if x == r - 1 and y == c - 1:
-            if (k - cnt) % 2 == 1: # 도착했는데 홀수개 남았을 때
+        x, y, path, cnt = q.popleft()
+        if x == r and y == c:  # 도착
+            # 도착했는데 홀수개 남았을 때 다른곳에 들렸다가 재방문 해야하는데 홀수번이면 이 자리로 다시 되돌아 올 수 없다.
+            if (k - cnt) % 2 == 1:
                 return "impossible"
             if cnt == k:
-                return tmp
+                return path
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if cnt + abs(r - x) + abs(c - y) > k:
-                break
-            if 0 <= nx < n and 0 <= ny < m:
-                q.append((nx, ny, tmp + dirs[i], cnt + 1))
+            if not (0 < nx <= n and 0 < ny <= m): continue  # 범위 체크
+            if cnt + 1 + abs(r - nx) + abs(c - ny) > k:  # 현재 위치까지 오는데 사용한 횟수 + 현재 위치로부터 종점까지 남은 거리가 주어진 횟수보다 크다면
+                continue
+            q.append((nx, ny, path + dirs[i], cnt + 1))
+            print(q)
+            break
 
     return "impossible"
