0bd34b2e09215fe90b1e6db3c0bbf1b3bbb79bd9
yujung7768903 <yu-jung31476@naver.com>
2023-06-30 19:05:23 +0900

    김유정: 광고삽입 문제 복습(23.06.30)

--- week3/풍선_터트리기/김유정_풍선_터트리기.java
+++ week3/풍선_터트리기/김유정_풍선_터트리기.java
@@ -1,6 +1,7 @@
 package week3.풍선_터트리기;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class 김유정_풍선_터트리기 {
     public static void main(String[] args) {
@@ -29,6 +30,26 @@ class 김유정_풍선_터트리기_Solution {
             }
         }
         
+        return answer;
+    }
+}
+
+// 시간 초과가 발생한 문제 풀이
+class 김유정_풍선_터트리기_Solution2 {
+    public int solution(int[] a) {
+        int answer = 2;
+        
+        for (int i = 1; i < a.length - 1; i++) {
+            // 기준 값의 왼쪽과 오른쪽 배열 각각에서 배열의 원소를 모두 돌며 비교해서 최솟값을 구해야함. 
+            // 시간복잡도: (0 + n - 1) + (1 + n - 2) + (2 + n - 3) + ... (n - 1 + 0) = (n - 1) * n = O(n^2)
+            int leftMin =  Arrays.stream(a, 0, i).parallel().min().getAsInt();
+            int rightMin = Arrays.stream(a, i + 1, a.length).parallel().min().getAsInt();
+            // int leftMin = IntStream.range(0, i).map(num -> a[num]).parallel().min().getAsInt();
+            // int rightMin = IntStream.range(i + 1, a.length).map(num -> a[num]).parallel().min().getAsInt();
+            if (a[i] < leftMin || a[i] < rightMin) {
+                answer++;
+            }
+        }
         return answer;
     }
 }
